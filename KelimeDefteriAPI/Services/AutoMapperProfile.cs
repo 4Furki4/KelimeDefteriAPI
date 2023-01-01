6606a1ec323df7a10694a84473acd0c600eb9d89
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-01-01 18:49:45 +0300

    Readme is now added. Oops...

---
+++ KelimeDefteriAPI/Services/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿    using AutoMapper;
+using KelimeDefteriAPI.Context.EntityConcretes;
+using KelimeDefteriAPI.Context.ViewModels;
+
+namespace KelimeDefteriAPI.Services
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<RecordViewModel, Record>();
+            CreateMap<WordViewModel, Word>().ForMember(word => word.Name, opt => opt.MapFrom(vm => vm.Name))
+                .ForMember(word => word.Definitions, opt => opt.MapFrom(vm => vm.Definitions));
+            CreateMap<DefinitionViewModel, Definition>()
+                .ForMember(def => def.Description, opt => opt.MapFrom(vm => vm.definition))
+                .ForMember(def => def.DescriptionType, opt => opt.MapFrom(vm => vm.definitionType));
+        }
+    }
+}
